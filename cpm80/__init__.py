7626b8dbd25b8680475854fa2b3c8a16a76a9176
Ivan Kosarev <mail@ivankosarev.com>
2024-08-03 15:40:05 +0100

    Represent disk formats with a class.

--- cpm80/__init__.py
+++ cpm80/__init__.py
@@ -7,6 +7,23 @@ import tty
 import z80
 
 
+class DiskFormat(object):
+    def __init__(self):
+        self.bls_block_size = 2048
+        self.spt_sectors_per_track = 40
+        self.bsh_block_shift_factor = 4
+        self.blm_allocation_block_mask = 15  # = 2**BSH - 1.
+        self.exm_extent_mask = 1  # EXM = 1 and DSM < 256 means BLS = 2048.
+        self.dsm_disk_size_max = 194  # In BLS units.
+        self.drm_max_dir_entry = 63
+        self.al0_allocation_mask = 128  # 1 block for 64 dirs, 32 bytes each.
+        self.al1_allocation_mask = 0
+        self.cks_directory_check_size = 16
+        self.off_system_tracks_offset = 2
+        self.removable = True
+        self.skew_factor = 0  # No translation.
+
+
 class _CPMMachineMixin(object):
     __REBOOT = 0x0000
     __BDOS = 0x0005
@@ -47,34 +64,20 @@ class _CPMMachineMixin(object):
         return addr
 
     def __set_up_disk_tables(self):
-        # TODO: Have a class describing disk parameters.
-        bls_block_size = 2048
-        spt_sectors_per_track = 40
-        bsh_block_shift_factor = 4
-        blm_allocation_block_mask = 15  # = 2**BSH - 1.
-        exm_extent_mask = 1  # EXM = 1 and DSM < 256 means BLS = 2048.
-        dsm_disk_size_max = 194  # In BLS units.
-        drm_max_dir_entry = 63
-        al0_allocation_mask = 128  # 1 block for 64 dirs, 32 bytes each.
-        al1_allocation_mask = 0
-        cks_directory_check_size = 16
-        off_system_tracks_offset = 2
-
-        removable = True
-        cks = (drm_max_dir_entry + 1) // 4 if removable else 0
+        f = DiskFormat()
 
         # Shared by all identical drives.
         dpb_disk_param_block = self.__allocate_disk_table_block(
-            spt_sectors_per_track.to_bytes(2, 'little') +
-            bsh_block_shift_factor.to_bytes(1, 'little') +
-            blm_allocation_block_mask.to_bytes(1, 'little') +
-            exm_extent_mask.to_bytes(1, 'little') +
-            dsm_disk_size_max.to_bytes(2, 'little') +
-            drm_max_dir_entry.to_bytes(2, 'little') +
-            al0_allocation_mask.to_bytes(1, 'little') +
-            al1_allocation_mask.to_bytes(1, 'little') +
-            cks_directory_check_size.to_bytes(2, 'little') +
-            off_system_tracks_offset.to_bytes(2, 'little'))
+            f.spt_sectors_per_track.to_bytes(2, 'little') +
+            f.bsh_block_shift_factor.to_bytes(1, 'little') +
+            f.blm_allocation_block_mask.to_bytes(1, 'little') +
+            f.exm_extent_mask.to_bytes(1, 'little') +
+            f.dsm_disk_size_max.to_bytes(2, 'little') +
+            f.drm_max_dir_entry.to_bytes(2, 'little') +
+            f.al0_allocation_mask.to_bytes(1, 'little') +
+            f.al1_allocation_mask.to_bytes(1, 'little') +
+            f.cks_directory_check_size.to_bytes(2, 'little') +
+            f.off_system_tracks_offset.to_bytes(2, 'little'))
 
         # Shared by all drives.
         dirbuf_scratch_pad = self.__allocate_disk_table_block(b'\x00' * 128)
@@ -83,9 +86,10 @@ class _CPMMachineMixin(object):
         bdos_scratch_pad1 = 0x0000
         bdos_scratch_pad2 = 0x0000
         bdos_scratch_pad3 = 0x0000
+        cks = (f.drm_max_dir_entry + 1) // 4 if f.removable else 0
         csv_scratch_pad = self.__allocate_disk_table_block(b'\x00' * cks)
         alv_scratch_pad = self.__allocate_disk_table_block(
-            b'\x00' * (dsm_disk_size_max // 8 + 1))
+            b'\x00' * (f.dsm_disk_size_max // 8 + 1))
 
         self.__disk_header_table = self.__allocate_disk_table_block(
             xlt_sector_translation_vector.to_bytes(2, 'little') +
@@ -97,17 +101,14 @@ class _CPMMachineMixin(object):
             csv_scratch_pad.to_bytes(2, 'little') +
             alv_scratch_pad.to_bytes(2, 'little'))
 
-        disk_size = (dsm_disk_size_max + 1) * bls_block_size
+        disk_size = (f.dsm_disk_size_max + 1) * f.bls_block_size
         self.__disk_image = bytearray(disk_size)
         self.__disk_image[:] = b'\xe5' * disk_size
-
-        skew_factor = 0  # No translation.
-        # self.__physical_sectors = tuple(range(spt_sectors_per_track))
 
         self.__disk_track = 0
         self.__disk_sector = 0
 
-        self.__sectors_per_track = spt_sectors_per_track
+        self.__sectors_per_track = f.spt_sectors_per_track
 
     @staticmethod
     def __load_data(path):
