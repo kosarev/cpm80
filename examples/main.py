b244a431aae49682bec0fed5440036da6fa5f11f
Ivan Kosarev <mail@ivankosarev.com>
2024-09-03 11:31:36 +0100

    Use temporary disk in the main.py example.

--- examples/main.py
+++ examples/main.py
@@ -4,4 +4,4 @@ import cpm80
 
 # Command-line arguments can be passed to the main() function
 # directly.
-cpm80.main(['save 0 a.txt', 'ren b.txt=a.txt', 'dir'])
+cpm80.main(['--temp-disk', 'save 0 a.txt', 'ren b.txt=a.txt', 'dir'])
